8e40e9aa5b509f6c41deace02e86d72e5cd960ea
Anurag <anuragkokitkar09@gmail.com>
2026-05-14 10:51:17 +0530

    Updated project files

---
+++ CRUD_API/server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect('mongodb://127.0.0.1:27017/studentDB')
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
+// Schema
+const studentSchema = new mongoose.Schema({
+
+    name: String,
+    age: Number,
+    course: String
+
+});
+
+// Model
+const Student = mongoose.model('Student', studentSchema);
+
+
+// CREATE
+app.post('/students', async (req, res) => {
+
+    const student = new Student(req.body);
+
+    await student.save();
+
+    res.send(student);
+
+});
+
+
+// READ
+app.get('/students', async (req, res) => {
+
+    const students = await Student.find();
+
+    res.send(students);
+
+});
+
+
+// UPDATE
+app.put('/students/:id', async (req, res) => {
+
+    const student = await Student.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+
+    res.send(student);
+
+});
+
+
+// DELETE
+app.delete('/students/:id', async (req, res) => {
+
+    await Student.findByIdAndDelete(req.params.id);
+
+    res.send("Student Deleted");
+
+});
+
+
+// Server
+app.listen(3000, () => {
+
+    console.log("Server Running on Port 3000");
+
+});
